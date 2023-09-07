944650f0728d3d582f8739be17033573aa7cdf6c
feberman42 <feberman@student.42berlin.de>
2023-09-07 14:59:23 +0200

    cleanup and refactor

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: feberman <feberman@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/12 14:36:32 by feberman          #+#    #+#             */
-/*   Updated: 2023/09/07 13:35:36 by feberman         ###   ########.fr       */
+/*   Updated: 2023/09/07 14:53:28 by feberman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@ int	main(int argc, char *argv[])
 {
 	int			*arr;
 	t_stacks	*stacks;
-	int			back;
 
 	arr = create_array(argc, argv);
 	if (!arr)
@@ -24,9 +23,8 @@ int	main(int argc, char *argv[])
 	stacks = create_stacks(arr);
 	sort_arr(arr);
 	sorted_precheck(stacks);
-	back = stacks->back;
 	presort(stacks);
-	sort(stacks, back);
+	sort(stacks);
 	// 	error_int();
 	// free(arr);
 	// free_stacks(stacks);
@@ -35,49 +33,38 @@ int	main(int argc, char *argv[])
 
 int	presort(t_stacks *stacks)
 {
-	int		section_size;
-	int		center;
 	int		value_index;
 	int		i;
 	t_node	*check;
 
-	section_size = (((stacks->back - stacks->front) + 1) / ((stacks->arr[0] / 40) + 4)) + 1;
-	center = ((stacks->back - stacks->front) / 2) + stacks->front;
 	i = 1;
-	while (center + ((i - 1) * section_size) <= stacks->back || center - ((i - 1) * section_size) >= stacks->front)
+	while (stacks->center + ((i - 1) * stacks->section_size) <= stacks->back || stacks->center - ((i - 1) * stacks->section_size) >= stacks->front)
 	{
-		check = 0;
-		while (check != *(stacks->a))
+		check = (*(stacks->a))->prev;
+		while (1)
 		{
 			value_index = get_index(stacks->arr, (*(stacks->a))->value);
-			if (value_index <= center && value_index >= center - (i * section_size) && value_index >= stacks->front)
-				ops_pb(stacks);
-			else if (value_index > center && value_index <= center + (i * section_size) && value_index <= stacks->back)
-			{
-				ops_pb(stacks);
-				ops_rb(stacks);
-			}
-			else
-			{
-				if (!check)
-					check = *(stacks->a);
-				ops_ra(stacks);
-			}
+			evaluate_index(value_index, stacks, i);
+			if (value_index == get_index(stacks->arr, check->value))
+				break ;
 		}
 		i++;
 	}
 	return (0);
 }
 
-int	sort(t_stacks *stacks, int back)
+int	sort(t_stacks *stacks)
 {	
+	// ft_printf("rotate to insertion point");
 	rotate_to(stacks, A, find_insertion_point(stacks));
+	// ft_printf("push");
 	while (*(stacks->b))
 	{
-		rotate_to(stacks, B, stacks->arr[back]);
+		rotate_to(stacks, B, stacks->arr[stacks->back]);
 		ops_pa(stacks);
-		back--;
+		stacks->back--;
 	}
+	// ft_printf("rearrange");
 	rotate_to(stacks, A, stacks->arr[1]);
 	return (1);
 }
