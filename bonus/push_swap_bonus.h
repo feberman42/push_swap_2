a2ee8bed084bd33f9a1996d3b9d6c62c726cde9a
feberman42 <feberman@student.42berlin.de>
2023-09-13 16:58:11 +0200

    cleanup

--- bonus/push_swap_bonus.h
+++ bonus/push_swap_bonus.h
@@ -6,7 +6,7 @@
 /*   By: feberman <feberman@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/12 14:32:19 by feberman          #+#    #+#             */
-/*   Updated: 2023/09/13 13:20:33 by feberman         ###   ########.fr       */
+/*   Updated: 2023/09/13 16:34:36 by feberman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,17 +63,13 @@ void			ops_sa(t_stacks *stacks);
 void			ops_sb(t_stacks *stacks);
 void			ops_ss(t_stacks *stacks);
 void			do_operation(char *ops, t_stacks *stacks);
-
-int				append_ops(t_stacks *stacks, t_ops ops);
-void			put_ops_out(t_ops ops);
-void			print_ops(t_stacks *stacks);
-void			remove_ops(t_stacks *stacks, t_node *ops);
+int				check(t_stacks *stacks);
 
 int				*create_array(int argc, char *argv[]);
 int				populate_arr(int argc, char *argv[], int *arr);
 void			sort_arr(int *arr);
 
-t_stacks		*create_stacks(int *arr);
+t_stacks		*create_stacks_bonus(int *arr);
 int				arr_to_stack(t_stacks *stacks, int *arr);
 int				has_doubles(int *arr);
 char			**ps_split(const char *s);
@@ -83,37 +79,10 @@ int				count_words(const char *s);
 int				wordlen(const char *s);
 int				split_string(char **split, const char *s, int wc);
 
-int				last_sorted_front(t_stacks *stacks);
-int				last_sorted_back(t_stacks *stacks);
-void			sorted_precheck(t_stacks *stacks);
-int				precheck(t_stacks *stacks);
-void			set_section_size(t_stacks *stacks);
-
-int				get_index(int *arr, int value);
-int				search_in_stack(t_node **stack, int value);
-int				get_cost(t_stacks *stacks);
-int				ft_abs(int value);
-int				rotate_to(t_stacks *stacks, t_stack stack, int value);
-void			move(t_stacks *stacks, t_stack stack, int moves);
-int				index_to_push(t_stacks *stacks, int index, int i);
-int				evaluate_index(int index, t_stacks *stacks, int i);
-void			look_back(\
-					t_stacks *stacks, int *value_index, t_node **check, int i);
-int				find_insertion_point(t_stacks *stacks);
-void			final_push(t_stacks *stacks);
-int				presort(t_stacks *stacks);
-int				sort(t_stacks *stacks);
-
-void			filter(t_stacks *stacks);
-void			filter_useless_rotate(t_stacks *stacks);
-void			filter_useless_reverse_rotate(t_stacks *stacks);
-void			filter_double_rotate(t_stacks *stacks);
-void			filter_double_reverse_rotate(t_stacks *stacks);
-
 void			*free_split(char **split, int size);
 void			free_list(t_node **node);
-void				free_bonus(t_stacks *stacks);
+void			free_bonus(t_stacks *stacks);
 
-void			error(t_stacks *stacks);
+void			error(t_stacks *stacks, char *ops);
 
 #endif
