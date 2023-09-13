769c93f8374970e55720185839747012e2e930cb
feberman42 <feberman@student.42berlin.de>
2023-09-13 16:58:04 +0200

    added check func

--- bonus/push_swap_bonus.c
+++ bonus/push_swap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: feberman <feberman@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/12 14:36:32 by feberman          #+#    #+#             */
-/*   Updated: 2023/09/13 13:55:27 by feberman         ###   ########.fr       */
+/*   Updated: 2023/09/13 16:34:44 by feberman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,18 +21,21 @@ int	main(int argc, char *argv[])
 	arr = create_array(argc, argv);
 	if (!arr)
 		return (1);
-	stacks = create_stacks(arr);
+	stacks = create_stacks_bonus(arr);
 	sort_arr(arr);
-	
 	while(1)
 	{
 		ops = get_next_line(0);
 		if (!ops)
 			break ;
 		do_operation(ops, stacks);
+		free(ops);
 	}
-	//evaluate
-	ft_putendl_fd("DONE\n", 1);
+	if (check(stacks))
+		ft_putendl_fd("OK", 1);
+	else
+		ft_putendl_fd("KO", 1);
+	free_bonus(stacks);
 	return (0);
 }
 
@@ -61,14 +64,49 @@ void	do_operation(char *ops, t_stacks *stacks)
 	else if (!ft_strncmp(ops, "ss\n", 3))
 		ops_ss(stacks);
 	else
-		error(stacks);
+		error(stacks, ops);
 }
 
-// int	check(t_stacks *stacks)
-// {
-// 	t_node	*trv;
+int	check(t_stacks *stacks)
+{
+	t_node	*trv;
+	int		i;
 
-// 	if (*(stacks->b))
-// 		return (0);
-		
-// }
+	if (*(stacks->b))
+		return (0);
+	trv = *(stacks->a);
+	i = 1;
+	while (i < stacks->arr[0])
+	{
+		trv = trv->next;
+		i++;
+		if (trv->value != stacks->arr[i])
+			return (0);
+	}
+	return (1);
+}
+
+t_stacks	*create_stacks_bonus(int *arr)
+{
+	t_stacks	*stacks;
+
+	stacks = malloc(sizeof(t_stacks));
+	if (!stacks)
+		return (0);
+	stacks->a = malloc(sizeof(t_node *));
+	stacks->b = malloc(sizeof(t_node *));
+	if (!stacks->a || !stacks->b)
+	{
+		free_bonus(stacks);
+		return (0);
+	}
+	*(stacks->a) = 0;
+	*(stacks->b) = 0;
+	if (!arr_to_stack(stacks, arr))
+	{
+		free_bonus(stacks);
+		return (0);
+	}
+	stacks->arr = arr;
+	return (stacks);
+}
