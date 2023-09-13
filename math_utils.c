d635e8c572b281bd7adbe18c2d21d3f88111d5b2
feberman42 <feberman@student.42berlin.de>
2023-09-13 14:03:17 +0200

    moved get_cost

--- math_utils.c
+++ math_utils.c
@@ -6,7 +6,7 @@
 /*   By: feberman <feberman@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/30 17:52:56 by feberman          #+#    #+#             */
-/*   Updated: 2023/09/13 09:56:10 by feberman         ###   ########.fr       */
+/*   Updated: 2023/09/13 13:24:06 by feberman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,4 +17,15 @@ int	ft_abs(int value)
 	if (value < 0)
 		value *= -1;
 	return (value);
+}
+
+int	get_cost(t_stacks *stacks)
+{
+	if (stacks->back - 1 >= stacks->front + 5)
+	{
+		return (ft_abs(search_in_stack(\
+			stacks->b, stacks->arr[stacks->back - 1])));
+	}
+	else
+		return (INT_MAX);
 }
--- stacks.c
+++ stacks.c
@@ -6,7 +6,7 @@
 /*   By: feberman <feberman@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/09 10:33:20 by feberman          #+#    #+#             */
-/*   Updated: 2023/09/13 10:21:11 by feberman         ###   ########.fr       */
+/*   Updated: 2023/09/13 13:24:01 by feberman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,17 +38,6 @@ t_stacks	*create_stacks(int *arr)
 	stacks->back = stacks->arr[0];
 	stacks->operations = 0;
 	return (stacks);
-}
-
-int	get_cost(t_stacks *stacks)
-{
-	if (stacks->back - 1 >= stacks->front + 5)
-	{
-		return (ft_abs(search_in_stack(\
-			stacks->b, stacks->arr[stacks->back - 1])));
-	}
-	else
-		return (INT_MAX);
 }
 
 int	search_in_stack(t_node **stack, int value)
